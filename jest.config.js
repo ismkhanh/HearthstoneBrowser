38af161738feac35336ff5c2af614be7d527dff4
ik24 <ikismailkhan024@gmail.com>
2026-08-17 13:15:23 +0400

    update dependencies and configs

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  // Libraries that ship untranspiled code: fast-image is TypeScript source,
+  // axios resolves to ESM under the react-native export condition.
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?@?react-native'
+      + '|@react-native-community'
+      + '|@react-navigation'
+      + '|@shopify/flash-list'
+      + '|@d11/react-native-fast-image'
+      + '|axios'
+      + ')/)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/__tests__/**',
+    '!src/test/**',
+  ],
 };
